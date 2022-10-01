334e667f9080f8dbaf52116ee8116dd713627851
Sahil Rajput <sahilrajput03@gmail.com>
2022-10-01 21:54:08 +0530

    Fix mongo connection

--- initMongo.js
+++ initMongo.js
@@ -1,26 +1,24 @@
-const { MongoClient } = require("mongodb");
+const {MongoClient} = require('mongodb')
 
-const URI = "mongodb://localhost:27017/?maxPoolSize=20&w=majority"; // Connection URI
-const DB_NAME = "testdb";
+const URI = 'mongodb://localhost:27017/?maxPoolSize=20&w=majority' // Connection URI
+const DB_NAME = 'testdb'
 
-const db = client.db(DB_NAME);
-const myCollection = db.collection("my_collection");
-
-const client = new MongoClient(URI); // Create a new MongoClient
+const client = new MongoClient(URI) // Create a new MongoClient
+const db = client.db(DB_NAME)
+const userCollection = db.collection('users')
 
 const connect = async () => {
-  try {
-    await client.connect(); // Connect the client to the server (optional starting in v4.7)
-    await db.command({ ping: 1 }); // Establish and verify connection
-  } catch (e) {
-    console.log("Ooops.. failed to connect to db ~Sahil");
-    console.dir(e);
-  }
-};
+	try {
+		await client.connect() // Connect the client to the server (optional starting in v4.7)
+		await db.command({ping: 1}) // Establish and verify connection
+	} catch (e) {
+		console.log('Ooops.. failed to connect to db ~SupermMan')
+		console.dir(e)
+	}
+}
 
 module.exports = {
-  client,
-  connect,
-  myCollectionName,
-  myCollection,
-};
+	client,
+	connect,
+	userCollection,
+}
