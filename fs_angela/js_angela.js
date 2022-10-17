0e290ab08a85b5178db9aa18a9c99bb82a1d5964
Vivek Negi <negivivek13@gmail.com>
2022-10-18 01:36:45 +0530

    code basics

---
+++ fs_angela/js_angela.js
@@ -0,0 +1,29 @@
+//Leap Year checker program
+
+function isLeap(year) {
+	if (year % 4 === 0) {
+		if (year % 100 === 0) {
+			if (year % 400 === 0) {
+				return 'Leap year.'
+			}
+
+			return 'Not leap Year'
+		} else {
+			return 'Leap Year.'
+		}
+	} else {
+		return 'Not leap year.'
+	}
+}
+isLeap(2020)
+
+//Cross check the invite list and guest
+var guestname = ["yuli", "juli", "buli", "zuli"];
+var naam = prompt("what ur name");
+if (guestname.includes(naam)){
+    alert("welcome")
+}
+else{
+    alert("next time")
+}
+//Fizz Buzz
