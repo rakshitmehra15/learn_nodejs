38834b77710967e70735ccba5cd668c6681b2c7b
Vivek Negi <negivivek13@gmail.com>
2022-10-18 20:23:21 +0530

    added some code

--- fs_angela/js_angela.js
+++ fs_angela/js_angela.js
@@ -18,12 +18,28 @@ function isLeap(year) {
 isLeap(2020)
 
 //Cross check the invite list and guest
-var guestname = ["yuli", "juli", "buli", "zuli"];
-var naam = prompt("what ur name");
-if (guestname.includes(naam)){
-    alert("welcome")
+var guestname = ['yuli', 'juli', 'buli', 'zuli']
+var naam = prompt('what ur name')
+if (guestname.includes(naam)) {
+	alert('welcome')
+} else {
+	alert('next time')
 }
-else{
-    alert("next time")
-}
+
 //Fizz Buzz
+var output = []
+var count = 1
+
+function fizzBuzz() {
+	if (count % 3 === 0 && count % 5 === 0) {
+		output.push('FizzBuzz')
+	} else if (count % 3 === 0) {
+		output.push('Fizz')
+	} else if (count % 5 === 0) {
+		output.push('Buzz')
+	} else {
+		output.push(count)
+	}
+	count++
+	console.log(output)
+}
