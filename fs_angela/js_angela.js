e0752f2eca12cfa2c65a31a43eae32e49513584a
Vivek Negi <negivivek13@gmail.com>
2022-11-18 13:43:56 +0530

    code added

--- fs_angela/js_angela.js
+++ fs_angela/js_angela.js
@@ -79,3 +79,22 @@ while (i >= 1) {
 	}
 	i--
 }
+
+//fibonacci generator
+
+function fibonacciGenerator(n) {
+	var output = []
+	if (n === 1) {
+		output = [0]
+	} else if (n === 2) {
+		output = [0, 1]
+	} else {
+		output = [0, 1]
+		for (var i = 2; i < n; i++) output.push(output[output.length - 2] + output[output.length - 1])
+	}
+
+	return output
+
+	output = fibonacciGenerator(1)
+	console.log(output)
+}
